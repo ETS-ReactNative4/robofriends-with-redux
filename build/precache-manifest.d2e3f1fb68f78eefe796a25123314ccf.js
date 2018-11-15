fb2c05a9b9a0b72c0c79f60ea4ecfb59afd0ba08
KMIHALY <kaloczy.mihaly@gmail.com>
2018-11-15 19:32:53 +0100

    package redux, react-redux added

---
+++ build/precache-manifest.d2e3f1fb68f78eefe796a25123314ccf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b720ae16a3f1404c3d02",
+    "url": "/static/js/main.b720ae16.chunk.js"
+  },
+  {
+    "revision": "8a860ff73019cb55e3c6",
+    "url": "/static/js/1.8a860ff7.chunk.js"
+  },
+  {
+    "revision": "b720ae16a3f1404c3d02",
+    "url": "/static/css/main.d4831bc2.chunk.css"
+  },
+  {
+    "revision": "8a860ff73019cb55e3c6",
+    "url": "/static/css/1.e07d9f4b.chunk.css"
+  },
+  {
+    "revision": "d1fe655461df66fc00c2357c02089b63",
+    "url": "/index.html"
+  }
+];
