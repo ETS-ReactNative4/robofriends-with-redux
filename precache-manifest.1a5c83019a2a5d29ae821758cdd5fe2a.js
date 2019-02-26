ea1e383d8ae52f21d3de3609cb948a535df52390
KMIHALY <kaloczy.mihaly@gmail.com>
2019-02-26 23:09:50 +0100

    Updates

---
+++ precache-manifest.1a5c83019a2a5d29ae821758cdd5fe2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-with-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9134e8134bd7899b69d7",
+    "url": "/robofriends-with-redux/static/js/runtime~main.9134e813.js"
+  },
+  {
+    "revision": "e11e6ab8cee0192b7979",
+    "url": "/robofriends-with-redux/static/js/main.e11e6ab8.chunk.js"
+  },
+  {
+    "revision": "fa7b30eba27cff7bc700",
+    "url": "/robofriends-with-redux/static/js/2.fa7b30eb.chunk.js"
+  },
+  {
+    "revision": "e11e6ab8cee0192b7979",
+    "url": "/robofriends-with-redux/static/css/main.f2b39401.chunk.css"
+  },
+  {
+    "revision": "fa7b30eba27cff7bc700",
+    "url": "/robofriends-with-redux/static/css/2.0e32d148.chunk.css"
+  },
+  {
+    "revision": "432e8017d2d199c004167442f5042c58",
+    "url": "/robofriends-with-redux/index.html"
+  }
+];
